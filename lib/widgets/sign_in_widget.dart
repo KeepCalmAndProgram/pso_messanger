d6e7c89af1025dda84c742ec5976d89dade73c02
Andrew Vyshnytskyi <awisznicki2000@gmail.com>
2021-01-05 21:57:50 +0200

    Code review

--- lib/widgets/sign_in_widget.dart
+++ lib/widgets/sign_in_widget.dart
@@ -1,19 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../helper/helper_functions.dart';
+import '../screens/chat_rooms_screen.dart';
+import '../services/auth.dart';
+import '../services/database.dart';
 import '../widgets/appbar_widget.dart';
 import '../configuration/app_text.dart';
 import '../configuration/app_path.dart';
 import '../configuration/app_colors.dart';
 
 class SignIn extends StatefulWidget {
-  final Function toggle;
-  SignIn(this.toggle);
+  final Function _toggle;
+
+  SignIn({
+    @required Function toggle,
+  }) : _toggle = toggle;
 
   @override
   _SignInState createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
+  final formKey = GlobalKey<FormState>();
+  AuthMethods authMethods = AuthMethods();
+  DatabaseMethods databaseMethods = DatabaseMethods();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  bool isLoading = false;
+  QuerySnapshot snapshotUserInfo;
+
+  bool signIn() {
+    if (formKey.currentState.validate()) {
+      HelperFunctions.saveUserEmailSharedPreference(
+          emailTextEditingController.text);
+
+      setState(() {
+        isLoading = true;
+      });
+
+      databaseMethods
+          .getUserByUserEmail(emailTextEditingController.text)
+          .then((value) {
+        snapshotUserInfo = value;
+        HelperFunctions.saveUserEmailSharedPreference(
+            snapshotUserInfo.documents[0].data["name"]);
+      });
+
+      authMethods
+          .signInWithEmailAndPassword(emailTextEditingController.text,
+              passwordTextEditingController.text)
+          .then((value) {
+        if (value != null) {
+          HelperFunctions.saveUserLoggedInSharedPreference(true);
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => ChatRoom()));
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +74,38 @@ class _SignInState extends State<SignIn> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                TextField(
-                  style: Theme.of(context).textTheme.bodyText1,
-                  decoration: InputDecoration(
-                    hintText: AppText.emailText,
-                  ),
-                ),
-                TextField(
-                  style: Theme.of(context).textTheme.bodyText1,
-                  decoration: InputDecoration(
-                    hintText: AppText.passwordText,
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (value) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(value)
+                              ? null
+                              : AppText.emailValidatorText;
+                        },
+                        controller: emailTextEditingController,
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                          hintText: AppText.emailText,
+                        ),
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        validator: (value) {
+                          return value.length > 6
+                              ? null
+                              : AppText.passwordValidatorText;
+                        },
+                        controller: passwordTextEditingController,
+                        style: Theme.of(context).textTheme.bodyText1,
+                        decoration: InputDecoration(
+                          hintText: AppText.passwordText,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Divider(height: 22),
@@ -48,19 +117,24 @@ class _SignInState extends State<SignIn> {
                   ),
                 ),
                 Divider(height: 22),
-                Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                        AppColors.firstShadeOfButtonColor,
-                        AppColors.secondShadeOfButtonColor
-                      ]),
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Text(
-                    AppText.signInText,
-                    style: Theme.of(context).textTheme.bodyText1,
+                GestureDetector(
+                  onTap: () {
+                    signIn();
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          AppColors.firstShadeOfButtonColor,
+                          AppColors.secondShadeOfButtonColor
+                        ]),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      AppText.signInText,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
                   ),
                 ),
                 Divider(height: 15),
@@ -85,7 +159,7 @@ class _SignInState extends State<SignIn> {
                     Text(AppText.dontHaveAccountText,
                         style: Theme.of(context).textTheme.bodyText2),
                     GestureDetector(
-                      onTap: () => widget.toggle(),
+                      onTap: () => widget._toggle(),
                       child: Container(
                         padding: EdgeInsets.symmetric(vertical: 8),
                         child: Text(
